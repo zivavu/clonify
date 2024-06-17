a820e366371bd30c37dba882b3328acded50df88
zivavu <zivavu@gmail.com>
2024-06-17 21:22:22 +0200

    :recycle: Album page improve

--- pages/album/[id].vue
+++ pages/album/[id].vue
@@ -8,39 +8,51 @@ const route = useRoute();
 const albumId = route.params.id;
 
 const { data: album } = await useFetch<Album>(`/api/album/${albumId}`);
+const albumTracksDurations = album?.value?.tracks.items.map(
+	(track) => track.duration_ms
+);
+
+const albumDuration = albumTracksDurations?.reduce((a, b) => a + b, 0);
 </script>
 
 <template>
 	<div
 		v-if="album"
-		class="w-full h-full bg-gradient-to-br from-[#33384276] to-primary px-10 py-6">
-		<div class="flex flex-col space-y-5">
-			<div>
-				<h2 class="text-3xl font-medium">
+		class="w-full relative min-h-full flex flex-col space-y-6 bg-gradient-to-br from-[#33384276] to-primary px-12 py-6">
+		<div class="flex flex-row items-end space-x-6">
+			<img
+				:src="album?.images[0]?.url"
+				alt="Album Art"
+				class="rounded-xl min-w-60 max-w-60 aspect-square" />
+
+			<div class="space-y-6">
+				<h2 class="font-bold text-8xl">
 					{{ album?.name }}
 				</h2>
-			</div>
 
-			<div class="flex flex-row items-center space-x-2">
-				<ArtistLink :artist="album?.artists[0]" />
-				<p class="text-muted-foreground">・</p>
-				<p class="text-sm font-thin text-secondary-foreground">
-					{{ new Date(album?.release_date).getFullYear() }}
-				</p>
-				<p class="text-muted-foreground">・</p>
-				<p class="text-sm font-thin text-secondary-foreground">
-					{{ album?.total_tracks }} tracks
-				</p>
+				<div class="flex flex-row items-center space-x-2">
+					<ArtistLink :artist="album?.artists[0]" />
+					<p class="text-muted-foreground">・</p>
+					<p class="text-sm font-light">
+						{{ new Date(album?.release_date).getFullYear() }}
+					</p>
+					<p class="text-muted-foreground">・</p>
+					<p class="text-sm font-light">{{ album?.total_tracks }} tracks</p>
+					<p class="text-muted-foreground">・</p>
+					<p v-if="albumDuration" class="text-sm font-light">
+						{{ formatTime(albumDuration, true) }}
+					</p>
+				</div>
 			</div>
+		</div>
 
-			<div>
-				<hr />
-				<Track
-					v-for="(track, index) in album.tracks.items"
-					:key="index"
-					:track="track as TrackType"
-					:trackIndex="index + 1" />
-			</div>
+		<div>
+			<hr class="mb-2" />
+			<Track
+				v-for="(track, index) in album.tracks.items"
+				:key="index"
+				:track="track as TrackType"
+				:trackIndex="index + 1" />
 		</div>
 	</div>
 </template>
--- utils/formatTime.ts
+++ utils/formatTime.ts
@@ -1,5 +1,21 @@
-export function formatTime(milliseconds: number): string {
+/**
+ * Formats milliseconds into a string representing minutes and seconds.
+ *
+ * @param milliseconds - The number of milliseconds to format.
+ * @param useMinSecLiterals - Whether to use the words "min" and "sec" for the units.
+ * @returns A string representing the formatted time.
+ */
+export function formatTime(
+	milliseconds: number,
+	useMinSecLiterals = false
+): string {
 	const minutes = Math.floor(milliseconds / 60000);
 	const seconds = Math.floor((milliseconds % 60000) / 1000);
-	return `${minutes}:${seconds.toString().padStart(2, '0')}`;
+
+	const minutesString = useMinSecLiterals ? ' min ' : ':';
+	const secondsString = useMinSecLiterals ? ' sec' : '';
+
+	return `${minutes}${minutesString}${seconds
+		.toString()
+		.padStart(2, '0')}${secondsString}`;
 }
